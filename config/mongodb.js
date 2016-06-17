ec46d15b74d01e158e5413907570ebc54323b543
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-16 23:16:10 -0500

    setup mongodb method

--- config/mongodb.js
+++ config/mongodb.js
@@ -5,3 +5,34 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+/**
+ * Module dependencies
+ */
+var logger = require('logger').logger;
+
+/**
+ * setupMongoDB
+ *
+ * @description Configures and initiates the connection with the NoSQL MongoDB database
+ *
+ * @param {string}      DBName      Name of the database to connect
+ */
+function setupMongoDB (DBName, HostUri){
+    /**
+     *  required packages
+     */
+    var mongoose = require('mongoose');
+
+    mongoose.connect(HostUri + DBName);
+    logger.info('Connecting to MongoDB server, database: ' + DBName);
+
+    var con = mongoose.connection;
+    // logger conexión con la base de datos
+    con.once('open', function () {
+        logger.info('Connected to MongoDB successfully!');
+    });
+
+}
+
+// Export the function that initialize all routes
+module.exports.setupMongoDB = setupMongoDB;
