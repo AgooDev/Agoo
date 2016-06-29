6f036338d14b6034f0b7ced1177420f539b3eada
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-29 02:53:49 -0500

    required packages and models, working on passport strategies

--- routes/auth.js
+++ routes/auth.js
@@ -5,3 +5,17 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+// Load required packages
+var logger = require('../config/logger').logger;
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+var BearerStrategy = require('passport-http-bearer').Strategy;
+
+// Load required models
+var UserDataModel = require('../models/users');
+var User = UserDataModel.User;
+var ClientDataModel = require('../models/clients');
+var Client = ClientDataModel.Client;
+var TokenDataModel = require('../models/tokens');
+var Token = TokenDataModel.Token;
