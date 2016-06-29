db93e30bcda1e3c8281704fcc503d87989592179
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-29 03:30:35 -0500

    permission basic crud route

---
+++ routes/permissions.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2016-present, Agoo.com.co <http://www.agoo.com.co>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+// Load required packages
+var logger = require('../config/logger').logger;
+var Permission = require('../models/permissions').Permission;
