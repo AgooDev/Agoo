54b042f35fe0b7cb0eb8d24a9d6faade85ad3b83
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-29 03:23:42 -0500

    working on server file error no SetupRouter method

--- routes/routes.js
+++ routes/routes.js
@@ -5,3 +5,34 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+/**
+ * Module dependencies
+ */
+var Logger = require('../config/logger');
+var logger = Logger.logger;
+var moment = require('moment');
+
+/**
+ * setupRouter
+ *
+ * @description Configure all routes on express router
+ *
+ * @param {express.Router}      router      The varaible router used by the server
+ */
+function SetupRouter (router){
+
+    // logger for all request will first hits this middleware
+    router.use(function (req, res, next) {
+        var now = moment(new Date());
+
+        var date = now.format("DD-MM-YYYY HH:mm");
+        logger.info('%s %s %s', req.method, req.url, date);
+        next();
+    });    /**
+     * ====================================================================
+     */
+}
+
+// Export the function that initialize all routes
+module.exports.SetupRouter = SetupRouter;
