6ea57f89c0835e77e70a1a2a26c63c465988c128
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-29 02:17:03 -0500

    roles schema

---
+++ models/roles.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2016-present, Agoo.com.co <http://www.agoo.com.co>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+// Load required packages
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Define our Roles schema
+var RolesSchema = new Schema({
+    name: String,
+    creationDate: Date,
+    lastEditionDate: Date,
+    enabled: Boolean
+},{ versionKey: false });
+
+// Export the Mongoose model
+module.exports.Roles = mongoose.model('Roles', RolesSchema);
