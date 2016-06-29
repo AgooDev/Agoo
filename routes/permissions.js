d7ffb6e32c0d7485ce26f153258696cc93f9df2f
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-06-29 03:42:51 -0500

    get permission methods

--- routes/permissions.js
+++ routes/permissions.js
@@ -8,3 +8,31 @@
 // Load required packages
 var logger = require('../config/logger').logger;
 var Permission = require('../models/permissions').Permission;
+
+// ENDPOINT: /permissions METHOD: GET
+exports.getPermissions = function(req, res){
+    // Use the 'Permission' model to find all Permissions
+    Permission.find(function (err, permis) {
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+        }
+        // success
+        res.json(permis);
+    });
+};
+
+// ENDPOINT: /permissions/:id METHOD: GET
+exports.getPermissionById = function(req, res){
+    // Use the 'Permission' model to find single Permission
+    Permission.findById(req.params.id, function (err, permi) {
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+        }
+        // success
+        res.json(permi);
+    });
+};
