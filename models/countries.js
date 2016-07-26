05c9b517fd491305112e97f387aec389296552aa
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-26 16:19:17 -0500

    final country model

--- models/countries.js
+++ models/countries.js
@@ -5,3 +5,21 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+// Load required packages
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Define our Country schema
+var CountrySchema = new Schema({
+    name: String,
+    sortname: String,
+    enabled: {
+        type: Boolean,
+        default: true
+    }
+},{
+    timestamps  : true
+});
+
+// Export the Mongoose model
+module.exports.Country = mongoose.model('Country', CountrySchema);
